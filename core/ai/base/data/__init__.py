7caa792847f2bf96307fbc89c5b05e0a51f593ea
tu <tu.nguyen.lam@gmail.com>
2026-03-03 16:26:55 +0700

    chore: cleanup code

--- core/ai/base/data/__init__.py
+++ core/ai/base/data/__init__.py
@@ -2,16 +2,8 @@
 
 from .base import BaseLoader
 from .item import DataItem
-from .registry import LoaderRegistry, LoaderMetadata
-from .factory import LoaderFactory
-from .registration import register_loader, unregister_loader
 
 __all__ = [
     "BaseLoader",
     "DataItem",
-    "LoaderRegistry",
-    "LoaderMetadata",
-    "LoaderFactory",
-    "register_loader",
-    "unregister_loader",
 ]
